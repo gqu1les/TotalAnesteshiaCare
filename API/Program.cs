b0a03241d3ef9373ba9726af0b00f150a0348af7
Guillermo Quiles <gquiles@gmail.com>
2025-07-11 09:45:04 -0400

    End if Section 2

---
+++ API/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(opt =>
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.MapControllers();
+// creating service scope ( using = garbage collector)
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync(); // create db if not already exist
+    await DbInitializer.SeedData(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error Ocurred During migration.");
+
+}
+
+app.Run();
