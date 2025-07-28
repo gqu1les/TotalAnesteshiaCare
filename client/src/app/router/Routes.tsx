2009e7216582ad1a2494654745eb627ab8b766d1
Guillermo Quiles <gquiles@gmail.com>
2025-07-28 19:35:33 -0400

    End of Session 8

--- client/src/app/router/Routes.tsx
+++ client/src/app/router/Routes.tsx
@@ -1,20 +1,21 @@
 import { createBrowserRouter } from "react-router";
 import App from "../layout/App";
-import HomePage from "../../features/activities/home/HomePage";
-import ActivityForm from "../../features/activities/form/ActivityForm";
+import HomePage from "../../features/home/HomePage";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import ActivityDetail from "../../features/activities/details/AcitivityDetails";
+import ActivityForm from "../../features/activities/form/ActivityForm";
+import AcitivityDetailPage from "../../features/activities/details/AcitivityDetailPage";
+
 
 export const router = createBrowserRouter([
-    {
+   {
         path: '/',
         element: <App />,
         children: [
             { path: '', element: <HomePage /> },
             { path: 'activities', element: <ActivityDashboard /> },
-            { path: 'activities/:id', element: <ActivityDetail /> },
+            { path: 'activities/:id', element: <AcitivityDetailPage /> },
+            { path: 'createActivity', element: <ActivityForm key='create' /> },
             { path: 'manage/:id', element: <ActivityForm /> },
-            { path: 'createActivity', element: <ActivityForm key='create' /> }
         ]
     }
 ])
